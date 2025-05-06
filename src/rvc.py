a486280f92f641152cf51e773e48a3fc29d58de2
NeoDev <neoforevershog@gmail.com>
2025-05-07 04:58:06 +0700

    Update rvc.py

--- src/rvc.py
+++ src/rvc.py
@@ -30,26 +30,6 @@ class Config:
         if torch.cuda.is_available():
             i_device = int(self.device.split(":")[-1])
             self.gpu_name = torch.cuda.get_device_name(i_device)
-            if (
-                    ("16" in self.gpu_name and "V100" not in self.gpu_name.upper())
-                    or "P40" in self.gpu_name.upper()
-                    or "1060" in self.gpu_name
-                    or "1070" in self.gpu_name
-                    or "1080" in self.gpu_name
-            ):
-                print("16 series/10 series P40 forced single precision")
-                self.is_half = False
-                for config_file in ["32k.json", "40k.json", "48k.json"]:
-                    with open(BASE_DIR / "src" / "configs" / config_file, "r") as f:
-                        strr = f.read().replace("true", "false")
-                    with open(BASE_DIR / "src" / "configs" / config_file, "w") as f:
-                        f.write(strr)
-                with open(BASE_DIR / "src" / "trainset_preprocess_pipeline_print.py", "r") as f:
-                    strr = f.read().replace("3.7", "3.0")
-                with open(BASE_DIR / "src" / "trainset_preprocess_pipeline_print.py", "w") as f:
-                    f.write(strr)
-            else:
-                self.gpu_name = None
             self.gpu_mem = int(
                 torch.cuda.get_device_properties(i_device).total_memory
                 / 1024
@@ -57,11 +37,7 @@ class Config:
                 / 1024
                 + 0.4
             )
-            if self.gpu_mem <= 4:
-                with open(BASE_DIR / "src" / "trainset_preprocess_pipeline_print.py", "r") as f:
-                    strr = f.read().replace("3.7", "3.0")
-                with open(BASE_DIR / "src" / "trainset_preprocess_pipeline_print.py", "w") as f:
-                    f.write(strr)
+            
         elif torch.backends.mps.is_available():
             print("No supported N-card found, use MPS for inference")
             self.device = "mps"
